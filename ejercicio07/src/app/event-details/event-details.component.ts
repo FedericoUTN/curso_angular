d798ce2f14884755e0480cc24f4567aae4962caa
Federico <hulalaoooo@gmail.com>
2022-09-23 16:20:50 -0300

    ejercicio07 terminado

--- ejercicio07/src/app/event-details/event-details.component.ts
+++ ejercicio07/src/app/event-details/event-details.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventDetailsComponent implements OnInit {
 
+  estiloEjercicio = {
+    clasesDiv : true
+  }
+
   event = {
     name: 'ngConf 2025',
     date: '3/1/2025',
