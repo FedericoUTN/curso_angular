b72525a41e6530d629a0c8e3f3a5b4ca86a31347
Federico <hulalaoooo@gmail.com>
2022-09-23 16:01:26 -0300

    ejercicio06 terminado

--- ejercicio06/src/app/parent/parent.component.ts
+++ ejercicio06/src/app/parent/parent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+ 
 }
