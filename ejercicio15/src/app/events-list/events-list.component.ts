5aa46b8c55e9edecddab3a4918b5ea6880a9b3f1
Federico <hulalaoooo@gmail.com>
2022-09-30 00:04:26 -0300

    ejercicio15 completo

--- ejercicio15/src/app/events-list/events-list.component.ts
+++ ejercicio15/src/app/events-list/events-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EventsService } from '../events.service';
+
 
 @Component({
   selector: 'app-events-list',
@@ -7,52 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsListComponent implements OnInit {
 
-  events = [
-    {
-      name: 'Angular Connect',
-      date: '9/26/2036',
-      time: '10am',
-      location: {
-        address: '1 London Rd',
-        city: 'London',
-        country: 'England'
-      }
-    },
-    {
-      name: 'ng-nl',
-      date: '4/15/2037',
-      time: '9am',
-      location: {
-        address: '127 DT ',
-        city: 'Amsterdam',
-        country: 'NL'
-      }
-    },
-    {
-      name: 'ng-conf 2037',
-      date: '4/15/2037',
-      time: '9am',
-      location: {
-        address: 'The Palatial America Hotel',
-        city: 'Salt Lake City',
-        country: 'USA'
-      }
-    },
-    {
-      name: 'UN Angular Summit',
-      date: '6/10/2037',
-      time: '8am',
-      location: {
-        address: 'The UN Angular Center',
-        city: 'New York',
-        country: 'USA'
-      }
-    },
-  ]
+  events : any;
 
-  constructor() { }
+  constructor(private eventsService : EventsService) { 
+    this.events = eventsService.getEvents();
+  }
 
   ngOnInit() {
+    this.events = this.eventsService.getEvents();
   }
 
 }
