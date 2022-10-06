e6a30098aa718283dc66650a8870e0da7d2698b1
Federico <hulalaoooo@gmail.com>
2022-10-06 19:14:14 -0300

    ejericicio39 completo

--- ejercicio39/src/app/movies/movies.component.ts
+++ ejercicio39/src/app/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-movies',
@@ -8,19 +9,32 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MoviesComponent implements OnInit {
 
-  movies = [
+  movies : Movie[]= [
     {name: "Star Wars", rating: 'R', id: 1},  
     {name: "Jaws", rating: 'PG', id: 2},  
     {name: "E.T.", rating: 'R', id: 3},  
     {name: "Teen Wolf", rating: 'G', id: 4},  
     {name: "Ferris Bueller's Day Off", rating: 'G', id: 5},  
   ]
-  currentMovie: any;
+  currentMovie: Movie;
+  observableMovie= of(this.movies);
   
   constructor(private route: ActivatedRoute) {
+
   }
   
   ngOnInit() {
+    //let id : number = +this.route.snapshot.paramMap.get('id');
+    this.route.paramMap.subscribe((movie)=>{
+      let id = +movie.get('id');
+      this.currentMovie = this.movies.find((mov)=>mov.id === id);
+    })
+    
   }
 
+}
+interface Movie {
+  name : string,
+  rating: string,
+  id: number
 }
