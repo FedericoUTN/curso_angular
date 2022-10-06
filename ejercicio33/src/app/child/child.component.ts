58d5ea176e5a471752ed7cf10a8e6399dea37ece
Federico <hulalaoooo@gmail.com>
2022-10-06 16:21:02 -0300

    ejercicio33 completo

--- ejercicio33/src/app/child/child.component.ts
+++ ejercicio33/src/app/child/child.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import {OnChanges} from '@angular/core'
+import { SimpleChanges } from '@angular/core';
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnChanges {
 
   @Input() quantity;
   squaredQuantity: number;
@@ -14,5 +15,7 @@ export class ChildComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  ngOnChanges(changes: SimpleChanges): void{
+    this.squaredQuantity = Math.pow(changes.quantity.currentValue, 2);
+  }
 }
