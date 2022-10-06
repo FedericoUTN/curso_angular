3ca703b974eeb04e36dbc7c2f832f4425f8cc89d
Federico <hulalaoooo@gmail.com>
2022-10-06 10:00:59 -0300

    ejercicio43 completo

--- ejercicio43/src/app/parent/parent.component.ts
+++ ejercicio43/src/app/parent/parent.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Observable, pipe, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-parent',
@@ -8,22 +9,27 @@ import { Subject } from 'rxjs';
 })
 export class ParentComponent implements OnInit {
 
-  subject;
+  subject: Subject<string> ;
   output = [];
   
   constructor() {
-    this.subject = new Subject();
+    this.subject = new Subject<string>();
    }
 
   ngOnInit() {
-    this.subject
+    this.logica(this.subject) 
     .subscribe(key => {
       this.output.push(key);
+      console.log(this.output);
     });
   }
 
   keypress(e) {
     this.subject.next(e.key);
   }
+
+  logica = pipe(
+    map((key:string)=> key.toUpperCase())
+  )
   
 }
